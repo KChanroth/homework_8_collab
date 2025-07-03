0d932119b719605fd8316b53195edfa22ede0963
HeangMengsea <2025024heang@aupp.edu.kh>
2025-07-03 16:36:41 +0700

    exercise 4 and 5

--- exercise-4.py
+++ exercise-4.py
@@ -1,2 +1,26 @@
+#chunking_by([5, 4, 7, 3, 4, 5, 4], 3) == [[5, 4, 7], [3, 4, 5], [4]]
+#chunking_by([3, 4, 5], 1) == [[3], [4], [5]]
+
 def chunking_by(numbers, chunck):
-    ...
+    #check for any issue with the provided variables
+    if len(numbers) <= 0:
+        return "The list is empyt"
+    if chunck <= 0:
+        return "Please enter a number higher than '0'"
+    elif isinstance(chunck, int) is False:
+        return "Please enter a whole number"
+
+    block = []
+    last_block = []
+    times = len(numbers)//chunck
+    for y in range(times):
+        block.append(numbers[0:chunck])
+        for x in numbers[0:chunck]:
+            numbers.remove(x)
+    if len(numbers) != 0:
+        for x in numbers:
+            last_block.append(x)
+        block.append(last_block)
+    return block
+
+print(chunking_by([3, 4, 5], 1))
--- exercise-5.py
+++ exercise-5.py
@@ -1,2 +1,37 @@
 def reverse_ascending(numbers):
-    ...
+    new_list = numbers.copy()
+    block = []
+    final_iteration = []
+    while len(new_list) != 0:
+        count = 1
+        for x in new_list:
+            try:
+                if x < new_list[new_list.index(x)+1]:
+                    count += 1
+                else:
+                    break
+            except:
+                count += 1
+
+        block.append(new_list[0:count])
+        for x in new_list[0:count]:
+            new_list.remove(x)
+    for x in block:
+        for y in x[::-1]:
+            final_iteration.append(y)
+    return final_iteration
+
+assert list(reverse_ascending([1, 2, 3, 4, 5])) == [5, 4, 3, 2, 1]
+assert list(reverse_ascending([5, 7, 10, 4, 2, 7, 8, 1, 3])) == [
+    10,
+    7,
+    5,
+    4,
+    8,
+    7,
+    2,
+    3,
+    1,
+]
+assert list(reverse_ascending([5, 4, 3, 2, 1])) == [5, 4, 3, 2, 1]
+assert list(reverse_ascending([])) == []
